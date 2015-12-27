c28d3586f5ad0f0cd0768d6408effba1f3c72a88
Jacob Brooks <jacob@chrisbrooks.org>
2015-12-27 15:40:40 -0800

    Django models, removed db

--- generator/models.py
+++ generator/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class GradeSheet(models.Model):
+  title = models.CharField(max_length=200)
+
+class Section(models.Model):
+  name = models.CharField(max_length=500)
+
+  possible = models.IntegerField()
+
+  gradeSheet = models.ForeignKey(GradeSheet)
+
+class Deducton(models.Model):
+  text = models.CharField(max_length=1000)
+
+  amount = models.IntegerField()
+
+  section = models.ForeignKey(Section)
