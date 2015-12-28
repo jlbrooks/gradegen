8f2a5d0cf0bc3f018a01cc3e9e74fe997d8c30c2
Jacob Brooks <jacob@chrisbrooks.org>
2015-12-27 16:01:52 -0800

    Partial deduction form

--- generator/forms.py
+++ generator/forms.py
@@ -4,16 +4,33 @@ from generator.models import GradeSheet, Section, Deduction
 class SectionForm(forms.Form):
   name = forms.CharField(max_length=500)
 
-  possible = forms.IntegerField(required=True)
+  possible = forms.IntegerField(required=True, min_value=1)
 
-  gradeSheetId = forms.IntegerField(required=True, min_value=1)
+  gradeSheetId = forms.CharField(required=True)
 
   def clean_gradeSheetId(self):
     gId = self.cleaned_data['gradeSheetId']
 
     try:
-      Section.objects.get(id=gId)
-    except:
+      GradeSheet.objects.get(id=gId)
+    except:required=True
       raise forms.ValidationError("GradeSheet doesn't exist.")
 
     return gId
+
+class DeductionForm(self):
+  text = forms.CharField(required=True, max_length=1000)
+
+  amount = forms.IntegerField(required=True)
+
+  sectionId = forms.CharField(required=True)
+
+  def clean_sectionId(self):
+    sId = self.cleaned_data['sectionId']
+
+    try:
+      Section.objects.get(id=sId)
+    except:required=True
+      raise forms.ValidationError("Section doesn't exist.")
+
+    return sId
